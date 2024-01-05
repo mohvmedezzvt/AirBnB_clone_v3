9b0de68772ed6aa1438f5b0f2469a64a940c60a3
devbn3li <fudwhc@gmail.com>
2024-01-05 23:17:28 +0200

    pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Index objects that handles all default RestFul API actions"""
-from flask import Blueprint
+"""sharing app_views Blueprint"""
 
+from flask import Blueprint
+from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__)
