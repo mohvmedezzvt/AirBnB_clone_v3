addf0e3ce652956bf35b55e3bada9d1bf87ffa63
devbn3li <fudwhc@gmail.com>
2024-01-05 22:43:35 +0200

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,19 +3,43 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import environ
+import os
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ 404 page not found """
+    return {"error": "Not found"}, 404
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """ Remove the current SQLAlchemy Session """
+def teardown_db(exception):
+    """ Teardown """
     storage.close()
 
 
+if os.getenv("HBNB_API_HOST"):
+    host = os.getenv("HBNB_API_HOST")
+else:
+    host = "0.0.0.0"
+
+if os.getenv("HBNB_API_PORT"):
+    port = int(os.getenv("HBNB_API_PORT"))
+else:
+    port = 5000
+
+
 if __name__ == "__main__":
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Index objects that handles all default RestFul API actions"""
-from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
+
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
-    """ Return status """
-    return jsonify({"status": "OK"})
+    return {
+        "status": "OK",
+    }
