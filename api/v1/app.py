f93a23bf98277c6fc95b4f52510e9bced99592c6
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-01-06 13:56:35 +0200

    trying fixing errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,44 +1,35 @@
 #!/usr/bin/python3
 """ Main file of the API """
-import os
 from flask import Flask
 from models import storage
+from os import environ
 from api.v1.views import app_views
 
-
-# creating a Flask app
 app = Flask(__name__)
-
-
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """ 404 error handler """
     return {"error": "Not found"}, 404
 
 
 @app.errorhandler(400)
 def page_not_found(e):
+    """ 400 error handler """
     message = e.description
     return message, 400
 
 
 @app.teardown_appcontext
 def close(self):
+    """ Close storage """
     storage.close()
 
 
-if os.getenv("HBNB_API_HOST"):
-    host = os.getenv("HBNB_API_HOST")
-else:
-    host = "0.0.0.0"
-
-if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT"))
-else:
-    port = 5000
-
-
 if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(environ.get('HBNB_API_PORT', 5000))
+
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
+    """ Return status """
     return {
         "status": "OK",
     }
