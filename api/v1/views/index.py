2183580d4e2b0c81cea7782242431b7531ff4e7c
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-01-07 14:36:11 +0200

    a new view for State objects that handles all default RESTFul API actions

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,4 +8,15 @@ from models import storage
 @app_views.route("/status", methods=["GET"])
 def status():
     """ Return status """
-    return jsonify({'status': 'OK'})
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    """ Return stats """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
